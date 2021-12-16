c3431856c7b269f4d24bb36c8ba2bbaf3a1f3d95
NAM TAE SIK <tesnam1216@gmail.com>
2021-12-17 00:32:03 +0900

    add refrigeratorPage Feature implementation

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -3,23 +3,25 @@ import LogInPage from "./page/logInPage";
 import MembershipPage from "./page/membershipPage";
 import ProductUpload from "./page/productUpload";
 import RefrigeratorPage from "./page/refrigeratorPage";
-import Nav from './page/nav'
-import Footer from './page/footer'
-import MainPage from './page/mainPage'
+import Nav from "./page/nav";
+import Footer from "./page/footer";
+import MainPage from "./page/mainPage";
 import AlarmPage from "./page/alarmPage";
-import MyPage from './page/myPage';
-import MyPageCorrection from './page/myPageCorrection'
+import MyPage from "./page/myPage";
+import MyPageCorrection from "./page/myPageCorrection";
 
 function App() {
-  return <>
-    <Nav />
-    {/* <MainPage /> */}
-    {/* <AlarmPage /> */}
-    {/* <MyPage /> */}
-    {/* <MyPageCorrection /> */}
-    <RefrigeratorPage />
-    <Footer />
-  </>;
+  return (
+    <>
+      <Nav />
+      {/* <MainPage /> */}
+      {/* <AlarmPage /> */}
+      {/* <MyPage /> */}
+      {/* <MyPageCorrection /> */}
+      <RefrigeratorPage />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
