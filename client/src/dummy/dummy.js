79f525306ef667222a9eb58e5e0a6653cbce7d86
NAM TAE SIK <tesnam1216@gmail.com>
2021-12-17 00:23:21 +0900

    add test file

---
+++ client/src/dummy/dummy.js
@@ -0,0 +1,45 @@
+export const dummy =
+{
+  "product": [
+    {
+      "id": 1,
+      "storage": "냉장실",
+      "category": "유제품",
+      "image": "../dummy_image/바나나우유.jpeg",
+      "name": "바나나우유",
+      "expiration_date": "2021-12-16-목"
+    },
+    {
+      "id": 2,
+      "storage": "냉장실",
+      "category": "어패류",
+      "image": "../dummy_image/가리비.jpeg",
+      "name": "가리비",
+      "expiration_date": "2021-12-18-토"
+    },
+    {
+      "id": 3,
+      "storage": "냉장실",
+      "category": "음료",
+      "image": "../dummy_image/기네스.jpeg",
+      "name": "기네스(맥주)",
+      "expiration_date": "2022-12-16-목"
+    },
+    {
+      "id": 4,
+      "storage": "냉장실",
+      "category": "완제품",
+      "image": "../dummy_image/맥스봉.png",
+      "name": "맥스봉(소세지)",
+      "expiration_date": "2022-12-16-목"
+    },
+    {
+      "id": 5,
+      "storage": "냉동실",
+      "category": "아이스크림",
+      "image": "../dummy_image/투게더.jpeg",
+      "name": "투게더(아이스크림)",
+      "expiration_date": "2022-12-16-목"
+    },
+  ]
+}
