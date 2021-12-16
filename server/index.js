97699febd52fd371d967046779aeaf0ded029960
LeeJaeYoung <87467464+dlwodud9529@users.noreply.github.com>
2021-12-16 15:11:29 +0900

    add client test

--- server/index.js
+++ server/index.js
@@ -6,23 +6,29 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 
-const controllers = require("./controllers");
+// const controllers = require("./controllers");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); //front에서 json 형식의 데이터를 보냈을 때 데이터 req.body에 넣어준다
+app.use(express.urlencoded({ extended: false })); // form submit 했을 때 데이터를 req.body에 넣어준다
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: true,
     credentials: true,
-    methods: ["GET", "POST", "OPTIONS"],
+    methods: ["GET", "POST", "DELETE"],
   })
 );
 app.use(cookieParser());
-app.post("/login", controllers.login);
-app.get("/accesstokenrequest", controllers.accessTokenRequest);
-app.get("/refreshtokenrequest", controllers.refreshTokenRequest);
 
-const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// app.get('/auth', controllers.auth);
+// app.post('/user', controllers.user);
+// app.post('/product', controllers.product);
+// app.post('/search', controllers.search);
+
+const HTTPS_PORT = process.env.HTTPS_PORT || 80;
 
 // 인증서 파일들이 존재하는 경우에만 https 프로토콜을 사용하는 서버를 실행합니다. 
 // 만약 인증서 파일이 존재하지 않는경우, http 프로토콜을 사용하는 서버를 실행합니다.
@@ -35,9 +41,11 @@ if(fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")){
   const credentials = { key: privateKey, cert: certificate };
 
   server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () => console.log("server runnning"));
+  server.listen(HTTPS_PORT, () => console.log("https server runnning"));
 
 } else {
-  server = app.listen(HTTPS_PORT)
+  server = app.listen(HTTPS_PORT, () => {
+      console.log("http server runnning")
+  })
 }
 module.exports = server;
