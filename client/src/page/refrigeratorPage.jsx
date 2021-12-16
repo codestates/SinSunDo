c107b11ba2a8d80a5fb26617b184ff3ecb9de0c5
NAM TAE SIK <tesnam1216@gmail.com>
2021-12-17 00:28:05 +0900

    add sort Product component and delete button implement and add total number count

--- client/src/page/refrigeratorPage.jsx
+++ client/src/page/refrigeratorPage.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./refrigeratorPage.module.css";
+import Product from "../components/product";
+import { dummy } from "../dummy/dummy";
 
 const RefrigeratorPage = () => {
+  const [product, setProduct] = useState(dummy.product);
+
+  const refrigerate = product.filter((item) => item.storage === "냉장실");
+  const freeze = product.filter((item) => item.storage === "냉동실");
+  const roomTemperature = product.filter((item) => item.storage === "실온");
+
+  const handleDelete = (id) => {
+    const filter = product.filter((el) => el.id !== id);
+    setProduct(filter);
+  };
+
   return (
     <>
       <button className={style.add}>추가</button>
@@ -15,29 +28,47 @@ const RefrigeratorPage = () => {
           <div className={style.countContainer}>
             <span className={style.total}>
               전체
-              <span className={style.total_num}>2</span>
+              <span className={style.total_num}>{refrigerate.length}</span>
             </span>
             <span className={style.danger}>
               위험
               <span className={style.danger_num}>3</span>
             </span>
           </div>
+
+          {refrigerate.length !== 0 ? (
+            product
+              .filter((item) => item.storage === "냉장실")
+              .map((el) => <Product product={el} handleDelete={handleDelete} />)
+          ) : (
+            <div className={style.empty}>
+              <div className={style.empty_sentence}>냉장실이 비어 있습니다</div>
+            </div>
+          )}
         </div>
         <div className={style.freezer}>
           <i className="freezer_logo fas fa-ice-cream"></i>
-
           <span className={style.freezer_name}>냉동실</span>
-
           <div className={style.countContainer}>
             <span className={style.total}>
               전체
-              <span className={style.total_num}>2</span>
+              <span className={style.total_num}>{freeze.length}</span>
             </span>
             <span className={style.danger}>
               위험
               <span className={style.danger_num}>3</span>
             </span>
           </div>
+          {/* 삼항연산자필요 */}
+          {freeze.length !== 0 ? (
+            product
+              .filter((item) => item.storage === "냉동실")
+              .map((el) => <Product product={el} handleDelete={handleDelete} />)
+          ) : (
+            <div className={style.empty}>
+              <div className={style.empty_sentence}>냉동실이 비어 있습니다</div>
+            </div>
+          )}
         </div>
         <div className={style.roomTemperature}>
           <i className="roomTemperature_logo fas fa-thermometer-half"></i>
@@ -47,13 +78,25 @@ const RefrigeratorPage = () => {
           <div className={style.countContainer}>
             <span className={style.total}>
               전체
-              <span className={style.total_num}>2</span>
+              <span className={style.total_num}>{roomTemperature.length}</span>
             </span>
             <span className={style.danger}>
               위험
               <span className={style.danger_num}>3</span>
             </span>
           </div>
+          {/* 삼항연산자필요 */}
+          {roomTemperature.length !== 0 ? (
+            product
+              .filter((item) => item.storage === "실온")
+              .map((el) => <Product product={el} />)
+          ) : (
+            <div className={style.empty}>
+              <div className={style.empty_sentence}>
+                실온보관이 비어 있습니다
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
