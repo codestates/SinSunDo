444e820e584fd8e68e71a2eb4e321150ce353fee
NAM TAE SIK <tesnam1216@gmail.com>
2021-12-17 15:48:44 +0900

    Add login Page Feature implementation

--- client/src/page/logInPage.jsx
+++ client/src/page/logInPage.jsx
@@ -1,22 +1,114 @@
-import React from "react";
+import React, { useCallback } from "react";
 import style from "./logInPage.module.css";
+import { useState } from "react";
+import axios from "axios";
+import MembershipPage from "./membershipPage";
 
-const LogInPage = () => {
+const LogInPage = ({ isLogin, setIsLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState(false);
+  const [membershipOnOff, setMembershipOnOff] = useState(false);
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const checkEmail = (email) => {
+    if (
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        email
+      )
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const handleLogin = () => {
+    const userinfo = { email, password };
+    // if (email === "") {
+    //   setMessage("이메일을 입력해 주세요");
+    // } else if (password === "") {
+    //   setMessage("비밀번호를 입력해 주세요");
+    // }
+
+    axios
+      .post(`${process.env.REACT_APP_SERVER_URL}/users/signup`, userinfo, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (res.message !== "ok") {
+          setMessage("고객님의 정보가 일치하지 않습니다");
+        } else {
+          setIsLogin(true);
+          setEmail("");
+          setPassword("");
+        }
+      });
+  };
+
+  const handleClick = useCallback(() => {
+    if (email === "") {
+      setMessage("이메일을 입력해주세요.");
+    } else if (!checkEmail(email)) {
+      setMessage("올바른 메일 양식으로 입력해주세요.");
+      return;
+    } else if (password === "") {
+      setMessage("비밀번호를 입력해주세요.");
+    } else {
+      setMessage("");
+      handleLogin();
+      return;
+    }
+  }, [email, password]);
+
+  const handlemembership = () => {
+    membershipOnOff === false
+      ? setMembershipOnOff(true)
+      : setMembershipOnOff(false);
+  };
+
   return (
-    <div className={style.container}>
-      <img className={style.logo} src="logo.png" />
-      <input className={style.email} type="text" placeholder="이메일" />
-      <input
-        className={style.password}
-        type="password"
-        placeholder="비밀번호"
-      />
-      <button className={style.login}>로그인</button>
-      <div className={style.membership}>
-        아직 sinsundo의 회원이 아니신가요 ?
+    <>
+      {membershipOnOff === true ? (
+        <MembershipPage handlemembership={handlemembership} />
+      ) : null}
+      <div className={style.container}>
+        <img className={style.logo} src="logo.png" />
+        <input
+          className={style.email}
+          type="text"
+          placeholder="   이메일"
+          onChange={onChangeEmail}
+        />
+        <input
+          className={style.password}
+          type="password"
+          placeholder="   비밀번호"
+          onChange={onChangePassword}
+        />
+        <button className={style.login} onClick={() => handleClick()}>
+          로그인
+        </button>
+        <span className={style.message}>{message}</span>
+
+        <button className={style.kakao}>카카오톡 로그인</button>
+        <button className={style.google}>구글 로그인</button>
+
+        <div className={style.membership}>
+          아직 sinsundo의 회원이 아니신가요 ?
+        </div>
+
+        <button className={style.membership_btn} onClick={handlemembership}>
+          회원가입
+        </button>
       </div>
-      <button className={style.membership_btn}>회원가입</button>
-    </div>
+    </>
   );
 };
 
