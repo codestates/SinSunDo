461b82cd28b00a48e7376eb1810708daf2243d62
Hong <gg9297@gmail.com>
2021-12-20 13:43:12 +0900

    Fix databases data

--- server/migrations/20211220031955-create-food.js
+++ server/migrations/20211220031955-create-food.js
@@ -8,14 +8,20 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      food_quantity: {
+        type: Sequelize.INTEGER
+      },
+      category_name: {
+        type: Sequelize.STRING
+      },
       storage: {
         type: Sequelize.STRING
       },
       food_name: {
         type: Sequelize.STRING
       },
-      food_quantity: {
-        type: Sequelize.INTEGER
+      day_ago: {
+        type: Sequelize.DATE
       },
       food_expiration: {
         type: Sequelize.DATE
