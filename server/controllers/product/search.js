51adad92737b3f2c0da418d17598d456f871b9c8
Hong <gg9297@gmail.com>
2021-12-20 09:52:24 +0900

    Merge feature/productSearch

--- server/controllers/product/search.js
+++ server/controllers/product/search.js
@@ -1,8 +1,33 @@
-const { product } = require('../../models/food')
-const { food_img } = require('../../models/food_img')
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
+const { food } = require('../../models')
 const { category } = require('../../models/category')
-const { expiration } = require('../../models/expiration')
+
 
 module.exports = (req, res) => {
+    const searchWord = req.query.searchWord
 
-}
+    food.findAll({
+        where:{
+            [Op.or]: [
+                {
+                    food_name: {
+                        [Op.like]: searchWord + "%"
+                    }
+                },
+                {
+                    food_expiration: {
+                        [Op.like]: searchWord + "%"
+                    }
+                }
+            ]
+        }
+    })
+    .then((data) => {
+        res.status(200).json({data: data, message: '상품 목록 가져오기를 성공하였습니다.'})
+    })
+    .catch( err => {
+        console.log(err)
+        res.status(500).json({message: 'error'})
+    })
+};
