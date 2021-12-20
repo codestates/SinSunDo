093adbce59bf2353bb1545ca3baae32a1533a72b
Hong <gg9297@gmail.com>
2021-12-20 15:05:14 +0900

    Fixed upstream data

--- server/controllers/users/mypage.js
+++ server/controllers/users/mypage.js
@@ -1,7 +1,32 @@
+// 내 정보 요청(마이페이지)
 const { users } = require('../../models');
 const { isAuthorized } = require('../tokenData/accessToken');
 const { generateRefreshToken, sendRefreshToken } = require('../tokenData/refreshToken');
 
-module.exports = (req, res) => {
+module.exports ={
+    // 마이페이지 정보 불러오기
+    get : (req, res) => {
+        const accessTokenData = isAuthorized(req);
+        if(!accessTokenData) {
+            res.status(401).send({ data: null, message: '유효하지 않은 토큰입니다.' });
+        }
+        else {
+            users.findOne({ where: {email: accessTokenData.email}})
+            .then((data) => {
+    
+                // const RefreshToken = generateRefreshToken(data.dataValues);
+                // sendRefreshToken(res, `jwt ${RefreshToken}`);
+    
+                delete userInfo.dataValues.password;
+                res.status(200).json({ "data" : { userInfo : data.dataValues }, message: '회원정보 조회에 성공했습니다'})
+            }).catch((err) => {
+                console.log(err);
+                res.status(500).send('');
+            });
+        }
+    },
+    // 마이페이지 토글(알람받기)정보 수정
+    post : (req, res) => {
 
+    }
 }
