35dd74d3024e66888cb6473c7da855419480047e
LeeJaeYoung <87467464+dlwodud9529@users.noreply.github.com>
2021-12-24 02:20:47 +0900

    Delete server directory

--- server/route/tokenData.js
+++
@@ -1,9 +0,0 @@
-// 참조 안하는 파일
-
-const router = require('express').Router()
-const controllers = require('../controllers/tokenData')
-
-//token router
-router.post('/refreshToken', controllers.refreshToken);
-
-module.exports = router
